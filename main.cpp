5fcbf184e5cf7293f971c8df2fd0deaf14379e74
Emil <ek@trailfinders.dk>
2025-02-26 20:55:14 +0100

    Added user story 2

--- main.cpp
+++ main.cpp
@@ -1,28 +1,43 @@
 #include <iostream>
-#include <fstream>
-#include <string>
 #include <vector>
+#include <string>
 
-class Task
-{
-	public:
-	std::string _description;
-	Task(std::string description){
-		_description = description;
-	}
+class Task {
+public:
+    std::string description;
+    bool completed;
+
+    Task(std::string desc) : description(desc), completed(false) {}
+
+    void markCompleted() {
+        completed = true;
+        std::cout << "Task completed: " << description << std::endl;
+    }
 };
 
-class TodoList
-{
-	
-	public:
-	std::vector<Task> todolist;
-	TodoList(){}
+class TodoList {
+public:
+    std::vector<Task> tasks;
+
+    void addTask(std::string description) {
+        tasks.push_back(Task(description));
+    }
+
+    void completeTask(int index) {
+        if (index >= 0 && index < tasks.size()) {
+            tasks[index].markCompleted();
+        } else {
+            std::cout << "Invalid task index." << std::endl;
+        }
+    }
 };
 
 int main() {
+    TodoList todo;
+    todo.addTask("Buy groceries");
+    todo.addTask("Finish homework");
 
-    TodoList todo = TodoList();
+    todo.completeTask(1); // Mark "Buy groceries" as completed
 
     return 0;
 }
